5ee81a2ba412ac3572fa1e99617138a4c907fbdd
Yordan Petrov <yordan.petroff@gmail.com>
2023-02-02 23:06:03 +0200

    All repositories created

---
+++ main/java/bg/softuni/mobilelele/configuration/DataInitializer.java
@@ -0,0 +1,13 @@
+package bg.softuni.mobilelele.configuration;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ main/java/bg/softuni/mobilelele/repository/UserRoleRepository.java
@@ -0,0 +1,9 @@
+package bg.softuni.mobilelele.repository;
+
+import bg.softuni.mobilelele.model.entity.UserRoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRoleRepository extends JpaRepository<UserRoleEntity, Long> {
+}
