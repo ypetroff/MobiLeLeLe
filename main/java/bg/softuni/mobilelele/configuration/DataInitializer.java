8cb418a53e6407603f752c2be487939e49591338
Yordan Petrov <yordan.petroff@gmail.com>
2023-02-02 23:34:51 +0200

    Added resources in the resources directory

--- main/java/bg/softuni/mobilelele/configuration/DataInitializer.java
+++ main/java/bg/softuni/mobilelele/configuration/DataInitializer.java
@@ -1,13 +1,31 @@
 package bg.softuni.mobilelele.configuration;
 
+import bg.softuni.mobilelele.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
 
+    private final BrandRepository brandRepository;
+    private final ModelRepository modelRepository;
+    private final OfferRepository offerRepository;
+    private final UserRepository userRepository;
+    private final UserRoleRepository userRoleRepository;
+
+    public DataInitializer(BrandRepository brandRepository, ModelRepository modelRepository,
+                           OfferRepository offerRepository, UserRepository userRepository,
+                           UserRoleRepository userRoleRepository) {
+        this.brandRepository = brandRepository;
+        this.modelRepository = modelRepository;
+        this.offerRepository = offerRepository;
+        this.userRepository = userRepository;
+        this.userRoleRepository = userRoleRepository;
+    }
+
     @Override
     public void run(String... args) throws Exception {
-
+//TODO: create a files to insert at least one representative of each entity table in the DB
+// add seed service and populate the DB
     }
 }
--- main/java/bg/softuni/mobilelele/model/entity/ModelEntity.java
+++ main/java/bg/softuni/mobilelele/model/entity/ModelEntity.java
@@ -23,16 +23,22 @@ public class ModelEntity extends BaseEntity {
     @Enumerated(EnumType.STRING)
     @Column
     private ModelCategory category;
+
     @Column(name = "image_url")
     private String imageUrl;
+
     @Column(name = "start_year")
     private Integer startYear;
+
     @Column(name = "end_year")
     private Integer endYear;
+
     @Column
     private LocalDateTime created;
+
     @Column
     private LocalDateTime modified;
+
     @ManyToOne
     private BrandEntity brand;
 }
