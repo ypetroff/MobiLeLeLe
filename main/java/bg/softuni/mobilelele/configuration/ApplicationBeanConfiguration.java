eadea3ede406ce6b0683b61df9f6572fc8c10510
Yordan Petrov <yordan.petroff@gmail.com>
2023-02-04 23:31:05 +0200

    imported model mapper and gson

--- main/java/bg/softuni/mobilelele/configuration/ApplicationBeanConfiguration.java
+++ main/java/bg/softuni/mobilelele/configuration/ApplicationBeanConfiguration.java
@@ -1,5 +1,8 @@
 package bg.softuni.mobilelele.configuration;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
